0da494c173a9c62feb70adf9aa802a4cd3f3a491
Kevin Ghadyani <Saturn2888@gmail.com>
2016-12-03 03:10:49 -0600

    Tweaked HomeView to be a functional component

--- src/code/views/home.jsx
+++ src/code/views/home.jsx
@@ -1,21 +1,13 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 
-// Utilities
-import StylesLoader from 'utilities/styles-loader'
+const HomeView = () => (
+	<div>
+		<h1>Ghadyani Framework Playground</h1>
+		<ul>
+			<li><Link to="/calendar" title="Calendar">Calendar</Link></li>
+		</ul>
+	</div>
+)
 
-// Styles
-const stylesLoader = StylesLoader.create()
-
-class Home extends PureComponent {
-	render() { return (
-		<div>
-			<h1>Ghadyani Framework Playground</h1>
-			<ul>
-				<li><Link to="/calendar" title="Calendar">Calendar</Link></li>
-			</ul>
-		</div>
-	)}
-}
-
-export default stylesLoader.render(Home)
+export default HomeView
