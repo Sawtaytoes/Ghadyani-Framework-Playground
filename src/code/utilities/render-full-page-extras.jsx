612e57bdf9e159e3a32ffa126fbfbef8104c09b1
Kevin Ghadyani <Saturn2888@gmail.com>
2016-12-03 02:19:20 -0600

    Added nicer fonts

--- src/code/utilities/render-full-page-extras.jsx
+++ src/code/utilities/render-full-page-extras.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export const htmlMeta = {
-	author: '',
-	copyright: '',
+	author: 'Kevin Ghadyani',
+	copyright: '© Kevin Ghadyani. All Rights Reserved.',
 	keywords: '',
 	msAppName: '',
 	msTileColor: '',
 	msTooltip: '',
-	titlePostfix: ''
+	titlePostfix: ' – Playground'
 }
 
 export const dnsPrefetches = [
@@ -15,6 +15,7 @@ export const dnsPrefetches = [
 ]
 
 export const thirdPartyAssets = [
-	// <link key="google-fonts" rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,400italic,700" />,
-	// <link key="font-awesome" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />,
+	<link key="google-fonts-roboto" rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,400italic,700" />,
+	<link key="google-fonts-pt-mono" rel="stylesheet" href="//fonts.googleapis.com/css?family=PT+Mono" />,
+	<link key="font-awesome" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />,
 ]
